2c57971875547e75baab33c820bfdfad3e7f41a3
nice-jpg <nice1997@foxmail.com>
2020-08-17 17:08:08 +0800

    access code ver.fin

--- access.py
+++ access.py
@@ -4,21 +4,21 @@ def solution(l):
     # Your code here
     l.sort()
     x = []
-    y = []
+    y = {}
     count = 0
     for li in l:
         for yi in y:
             if li % yi == 0:
-                count += 1
+                count += y[yi]
         for xi in x:
             if li % xi == 0:
-                y.append(li)
+                y[li] = y.get(li, 0) + 1
         x.append(li)
     return count
 
 
 l1 = [1, 1, 1]
-l2 = [1, 2, 3, 4, 5, 6, 6]
+l2 = [1, 2, 3, 4, 5, 6]
 l3 = [6, 6, 6, 12, 18]
 l4 = [1, 3, 5, 7]
 l5 = [6, 5, 4, 3, 2, 1]
